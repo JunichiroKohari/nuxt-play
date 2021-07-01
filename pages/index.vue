37fc4bb2bb58ddf1699a0b5b0bd4d46f636ee272
JunichiroKohari <junichiro@kohari.jp>
2021-07-01 21:28:00 +0900

    install pug

--- pages/index.vue
+++ pages/index.vue
@@ -21,6 +21,14 @@
           >
             Continue
           </v-btn>
+          <v-btn
+            id="donwloadbtn"
+            color="primary"
+            nuxt
+            @click="download"
+          >
+            download
+          </v-btn>
         </v-card-actions>
       </v-card>
     </v-col>
@@ -39,14 +47,35 @@ export default {
   },
   data () {
     return {
-      test:{}
+      test:{},
+      blobData: {}
     }
   },
   async mounted() {
     // const { data } = await axios.get('http://localhost/api/test')
-    const {data} = await axios.get('https://c57e447d-3719-4893-93d8-f589f3431417.mock.pstmn.io//v1/test')
-    console.log(data)
-    this.test = data
+    // const {data} = await axios.get('https://c57e447d-3719-4893-93d8-f589f3431417.mock.pstmn.io//v1/test')
+    // const {data} = await axios.get('http://localhost/v1/test')
+    // // console.log(data)
+    // this.blobData = new Blob([data], { type: 'image/jpeg' })
+    // document.getElementById("donwloadbtn").href = window.URL.createObjectURL(this.blobData);
+    // this.test = data
+  },
+  methods: {
+    download () {
+      var fname = "test.jpeg"
+        if (window.navigator.msSaveBlob) { 
+            console.log("#-msSaveBlob")
+            window.navigator.msSaveBlob(this.blobData, fname ); 
+            window.navigator.msSaveOrOpenBlob(this.blobData, fname ); 
+            const bloburl = window.URL.createObjectURL(this.blobData);
+        } else {
+            console.log("#-msSaveBlob-false")
+            const bloburl = window.URL.createObjectURL(this.blobData);
+            document.getElementById("donwloadbtn").href = bloburl;
+            // console.log(document.getElementById("donwloadbtn").href)
+            console.log(bloburl)
+        } 
+    }
   }
 }
 </script>
